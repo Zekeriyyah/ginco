2e15becf4196225b73129fd7385b88eb6aa18519
go-dev <awwalcodestar@gmail.com>
2025-03-15 12:44:29 +0100

    feat(server/main): loaded auth routes with server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,15 +4,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zekeriyyah/ginco/internal/database"
+	"github.com/zekeriyyah/ginco/internal/routes"
+	"github.com/zekeriyyah/ginco/pkg"
 )
 
 func main() {
-
+	database.InitDB()
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	routes.UserRoutes(r)
+
+	if err := r.Run(); err != nil { // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+		pkg.Error("server connection failed", err)
+	}
+
+	pkg.Info("Server successfully running on port 8080")
 }
