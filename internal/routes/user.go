aeb6dbd06212aa8bb23a4a76bb85fc4ebd5fdad6
go-dev <awwalcodestar@gmail.com>
2025-03-15 12:50:55 +0100

    feat(route-user): added user signup and login route

---
+++ internal/routes/user.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zekeriyyah/ginco/internal/handlers"
+	"github.com/zekeriyyah/ginco/internal/middleware"
+)
+
+func UserRoutes(r *gin.Engine) {
+	// Auth routes
+	r.POST("/signup", handlers.Signup)
+	r.POST("/login", handlers.Login)
+
+	// protected routes
+	routes := r.Group("/")
+	routes.Use(middleware.AuthMiddleware())
+	//routes.GET("/profile", handlers.GetUserProfile)
+}
