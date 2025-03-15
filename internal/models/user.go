c38607402cd81d303d491df743d0b675f2840cc7
go-dev <awwalcodestar@gmail.com>
2025-03-15 12:48:48 +0100

    feat(user-model): implement methods to set password on signup and verify on login

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,6 +3,7 @@ package models
 import (
 	"time"
 
+	"github.com/zekeriyyah/ginco/pkg"
 	_ "gorm.io/gorm"
 )
 
@@ -14,4 +15,19 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt time.Time `gorm:"index" json:"-"`
+}
+
+func (u *User) SetPassword(password string) error {
+	hashed, err := pkg.HashPassword(password)
+	if err != nil {
+		pkg.Error("failed to hash password", err)
+		return err
+	}
+
+	u.Password = string(hashed)
+	return nil
+}
+
+func (u *User) VerifyPassword(password string) bool {
+	return pkg.CheckPassword(u.Password, password)
 }
